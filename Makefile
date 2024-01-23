266de84e7899078ec8207eb19ffc068f3142250b
FusionSid <siddheshadsv@icloud.com>
2024-01-23 13:34:14 +1300

    feat: add current.cpp with a makefile to run it

---
+++ Makefile
@@ -0,0 +1,4 @@
+all:
+	@g++-13 -Wall -std=gnu++17 -O2 -o current current.cpp -lm
+	@./current < input.txt
+	@rm ./current
---
+++ current.cpp
@@ -0,0 +1,8 @@
+// Write code in this file, once done copy contents into dir
+#include <bits/stdc++.h>
+
+using std::cin, std::cout, std::string;
+int main()
+{
+    return 0;
+}
