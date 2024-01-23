53ac30fce0301f23b140c50376b9147444a17e28
FusionSid <siddheshadsv@icloud.com>
2024-01-23 13:39:23 +1300

    feat: complete 2019r1 House Of Cards

---
+++ 2019/round1/hoc.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using std::cin, std::cout;
+int main()
+{
+    int n;
+    cin >> n;
+
+    // using 1 + 2 + ... + n = n(n+1) / 2 (the triangle number)
+    int platforms = ((n - 1) * (n)) / 2;
+    // s(n) = 1n^2 + 1n (as it is a quadratic sequence)
+    int struts = pow(n, 2) + n;
+
+    cout << platforms + struts;
+
+    return 0;
+}
