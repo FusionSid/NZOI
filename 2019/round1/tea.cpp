4a2272ddc63a5510f996f401d34ddb78ec8f2ec2
FusionSid <siddheshadsv@icloud.com>
2024-01-23 13:39:47 +1300

    feat: complete 2019r1 Adding Teas

---
+++ 2019/round1/tea.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+const char order[] = {
+    'G',
+    'C',
+    'E',
+    'P',
+    'L',
+    'S',
+};
+
+using std::cin, std::cout, std::map, std::string;
+int main()
+{
+    map<char, int> tea_count{{'G', 0}, {'C', 0}, {'E', 0}, {'P', 0}, {'L', 0}, {'S', 0}};
+
+    while (true)
+    {
+        string input;
+        std::getline(cin, input);
+
+        if (input == "D")
+            break;
+
+        char tea = input[0];
+        int amount = std::stoi(input.substr(2, input.length() - 2));
+        tea_count[tea] += amount;
+    }
+
+    for (auto tea : order)
+        cout << tea_count[tea] << (tea == 'S' ? "" : " ");
+
+    return 0;
+}
