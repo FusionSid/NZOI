d18dbb43140c043cb33e3ac24b468d9795155aa4
FusionSid <siddheshadsv@icloud.com>
2024-01-25 23:36:43 +1300

    feat: complete 2019r2 More Tea Please

---
+++ 2019/round2/more_tea.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using std::cin, std::cout, std::string;
+
+const string TEAS[] = {"Ginger", "Chamomile", "Earl Gray", "Peppermint", "Lemon", "Strawberry"};
+const int BAGS_IN_CARTON = 24 * 10;
+
+int main()
+{
+    for (auto tea : TEAS)
+    {
+        int cartons;
+        cin >> cartons;
+        cout << tea << " " << cartons * BAGS_IN_CARTON << "\n";
+    }
+
+    return 0;
+}
