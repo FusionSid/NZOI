21f998f754f45c8de0d4407244ba18c9f1eddbea
FusionSid <siddheshadsv@icloud.com>
2024-02-03 21:24:21 +1300

    feat: complete NZOI Practice Set Perimeters

---
+++ practice_problems/perimeters.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using std::cin, std::cout, std::string;
+int main()
+{
+    int x, y, c, d;
+    cin >> x >> y >> c >> d;
+
+    int manhattan_dist = abs(x - c) + abs(y - d);
+    cout << manhattan_dist * 2;
+
+    return 0;
+}
