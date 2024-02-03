7c9810a5797ea296a0e44e207235a6dcac0759ac
FusionSid <siddheshadsv@icloud.com>
2024-02-03 21:24:41 +1300

    feat: complete NZOI Practice Set My Favourite Pet

---
+++ practice_problems/my_fav_pet.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using std::cin, std::cout, std::string;
+int main()
+{
+    int max_pet = -1;
+    int max_pet_index = 0;
+
+    int n;
+    for (int i = 0; i < 9; i++)
+    {
+        cin >> n;
+
+        if (n > max_pet)
+        {
+            max_pet_index = i;
+            max_pet = n;
+        }
+    }
+    cout << "Pet " << max_pet_index + 1 << "\n";
+    return 0;
+}
