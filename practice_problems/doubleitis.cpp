b6e484fb3ff5a341fb5913a49f0e0add6941f5a8
FusionSid <siddheshadsv@icloud.com>
2024-02-03 21:24:04 +1300

    feat: complete NZOI Practice Set Double-Itis

---
+++ practice_problems/doubleitis.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using std::cin, std::cout, std::string;
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    int actual_age = floor(n / 2);
+    if (actual_age < m)
+    {
+        cout << "No";
+    }
+    else
+    {
+        cout << "Yes";
+    }
+    return 0;
+}
