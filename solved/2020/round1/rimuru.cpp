c158789c194da1598b8b9a9c9a6b80cde8800e56
FusionSid <siddheshadsv@icloud.com>
2024-02-06 12:23:11 +1300

    feat: complete 2020r1: Rimuru's Road

---
+++ solved/2020/round1/rimuru.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using std::cin, std::cout, std::string;
+int main()
+{
+    double bar, outpost, inn, d;
+    cin >> bar >> outpost >> inn >> d;
+
+    int nearest_bar = abs(round(d / bar) * bar - d);
+    int nearest_outpost = abs(round(d / outpost) * outpost - d);
+    int nearest_inn = abs(round(d / inn) * inn - d);
+
+    int closest = std::min(std::min(nearest_bar, nearest_outpost), nearest_inn);
+
+    int amount_of_min = 0;
+    for (auto i : {nearest_bar, nearest_inn, nearest_outpost})
+    {
+        if (i == closest)
+        {
+            amount_of_min++;
+        }
+    }
+    cout << closest << "\n";
+    if (amount_of_min != 1)
+        cout << "can't make up my mind\n";
+
+    return 0;
+}
