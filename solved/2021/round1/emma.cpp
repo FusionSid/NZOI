3c8b781b356cdc6d716131946ac5161f1eba6b80
FusionSid <siddheshadsv@icloud.com>
2024-02-13 22:43:50 +1300

    feat: complete 2021r1: Emma's Socks

---
+++ solved/2021/round1/emma.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using std::cin, std::cout, std::string;
+int main()
+{
+    std::map<string, int> sock_count({{"Red", 0}, {"Pink", 0}, {"Purple", 0}, {"Blue", 0}});
+
+    string sock;
+    for (int i = 0; i < 7; i++)
+    {
+        cin >> sock;
+        sock_count[sock]++;
+    }
+
+    for (auto &p : sock_count)
+    {
+        if (p.second & 1)
+        {
+            cout << p.first;
+        }
+    }
+
+    return 0;
+}
