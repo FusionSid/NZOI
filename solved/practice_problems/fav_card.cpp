06d7f389ab594000b066aa02cfdb3c3ac3790861
FusionSid <siddheshadsv@icloud.com>
2024-02-17 23:02:04 +1300

    feat: complete NZPC Practice Problems: Favourite Card

---
+++ solved/practice_problems/fav_card.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using std::cin, std::cout, std::string;
+int main()
+{
+    int n, s, c;
+    cin >> n >> s;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> c;
+        if (c == s)
+        {
+            cout << i;
+            break;
+        }
+    }
+    return 0;
+}
