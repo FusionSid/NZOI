b8e477c6be15d35795706d3149f5dfa7cc5960e2
FusionSid <siddheshadsv@icloud.com>
2024-02-19 13:40:57 +1300

    feat: complete NZPC Practice Problems: Hopscotch

---
+++ solved/practice_problems/hopscotch.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define impossiblecheck               \
+    if (state != 'S' && state != 'E') \
+    {                                 \
+        cout << "impossible\n";       \
+        return 0;                     \
+    }
+
+using std::cin, std::cout, std::string;
+int main()
+{
+    int n;
+    char state, prev_state;
+
+    cin >> n;
+    cin >> prev_state;
+    state = prev_state;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        prev_state = state;
+        cin >> state;
+
+        switch (prev_state)
+        {
+        case 'S':
+            impossiblecheck break;
+        case 'E':
+            if (state != 'N')
+            {
+                cout << "impossible\n";
+                return 0;
+            }
+            break;
+        case 'N':
+            impossiblecheck break;
+        }
+    }
+    cout << "plausible\n";
+}
