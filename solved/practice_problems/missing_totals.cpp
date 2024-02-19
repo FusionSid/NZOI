f11f0791117941c90e15e80ce12a0e0701f783ef
FusionSid <siddheshadsv@icloud.com>
2024-02-19 18:01:01 +1300

    feat: complete NZPC Practice Problems: Missing Totals

---
+++ solved/practice_problems/missing_totals.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using std::cin, std::cout, std::string;
+int main()
+{
+    int n, price, discount;
+    cin >> n;
+
+    int total = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> price >> discount;
+        if (price > discount)
+        {
+            total += price - discount;
+        }
+    }
+
+    cout << total;
+
+    return 0;
+}
