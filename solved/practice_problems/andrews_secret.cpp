3a78925c8de8d419829e91ac33e13b605829f67b
FusionSid <siddheshadsv@icloud.com>
2024-02-20 21:06:24 +1300

    feat: complete NZPC Practice Problems: Andrew's Secret

---
+++ solved/practice_problems/andrews_secret.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using std::cin, std::cout, std::string;
+int main()
+{
+    
+    int n;
+    int min = 100000000;
+    int max = -1;
+    int total = 0;
+
+    for (int i = 0; i < 100; i++)
+    {
+        cin >> n;
+        min = std::min(min, n);
+        max = std::max(max, n);
+        total += n;
+    }
+
+    cout << min << " " << max << " " << total / 100;
+    return 0;
+}
