0597f4ac30fb3a9d7161437951702454ca0a9ca2
FusionSid <siddheshadsv@icloud.com>
2024-02-20 21:06:56 +1300

    feat: complete NZPC Practice Problems: Anagram Detection

---
+++ solved/practice_problems/anagram.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using std::cin, std::cout, std::string;
+int main()
+{
+    string word1;
+    string word2;
+
+    getline(cin, word1);
+    getline(cin, word2);
+
+    std::sort(word1.begin(), word1.end());
+    std::sort(word2.begin(), word2.end());
+
+    bool is_anagram = word1 == word2;
+    cout << (is_anagram ? "yes" : "no");
+
+    return 0;
+}
